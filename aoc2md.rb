445cf3a5f3f44c2aacea6071d8026f4aa6971ff3
Kevin Robayna <me@kevinrobayna.com>
2024-12-03 12:31:08 +0000

    Brew formula update for aoc2md version v0.1.5

--- aoc2md.rb
+++ aoc2md.rb
@@ -5,20 +5,20 @@
 class Aoc2md < Formula
   desc "The aoc2md tool to generate project setup 🎄"
   homepage "https://kevinrobayna.com"
-  version "0.1.4"
+  version "0.1.5"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.4/aoc2md_Darwin_arm64.tar.gz"
-      sha256 "6b8db3d2862bb90a80a08472bffd42220e9801415f980dd36bf253ad9be89c1c"
+    on_intel do
+      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.5/aoc2md_Darwin_x86_64.tar.gz"
+      sha256 "f4ad24a799edd3d7e6275577c914135d8d2f8944a47d7800a38960a1f2e36f24"
 
       def install
         bin.install "aoc2md"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.4/aoc2md_Darwin_x86_64.tar.gz"
-      sha256 "bd6fb60bce3488ed17eb8deba9f56e5438da95c1f2aaca8d7f127590184bf0d6"
+    on_arm do
+      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.5/aoc2md_Darwin_arm64.tar.gz"
+      sha256 "b145a9a709efe89710b25cd288585b43c1531b2db93205a3df23466b55344610"
 
       def install
         bin.install "aoc2md"
@@ -27,20 +27,24 @@ class Aoc2md < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.4/aoc2md_Linux_arm64.tar.gz"
-      sha256 "aff22a2ef5ccc4155d394bcbc81ec124f8d2f35b51f9a7c0f980e4b2b6accfd3"
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.5/aoc2md_Linux_x86_64.tar.gz"
+        sha256 "cf706c91292b979db006f8cda931b14adad54da34695ed18feedaddd8035daa8"
 
-      def install
-        bin.install "aoc2md"
+        def install
+          bin.install "aoc2md"
+        end
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.4/aoc2md_Linux_x86_64.tar.gz"
-      sha256 "2fd3e1cf2c576b1c4fad3859f8186a803bf092b2c4f7a9a1b6472c65452f1f76"
+    on_arm do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/kevinrobayna/aoc2md/releases/download/v0.1.5/aoc2md_Linux_arm64.tar.gz"
+        sha256 "c6bc3b6d1042e6a0211976096d1fba4c9509beb96303d80a250839b1ca244ab0"
 
-      def install
-        bin.install "aoc2md"
+        def install
+          bin.install "aoc2md"
+        end
       end
     end
   end
